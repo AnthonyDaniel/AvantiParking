13ffde0e9e41ad059f275c5fc58d024ea4bf9a76
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2019-09-25 12:20:02 -0600

    Update Headquarter_Controller.java

--- BackEndAvantiParking/Avantiparking/src/main/java/com/avantiparking/controller/Headquarter_Controller.java
+++ BackEndAvantiParking/Avantiparking/src/main/java/com/avantiparking/controller/Headquarter_Controller.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.avantiparking.exception.Resource_Not_Found;
 import com.avantiparking.model.Headquarter;
@@ -23,6 +25,8 @@ import com.avantiparking.repository.Headquarter_Repository;
 
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,
+		RequestMethod.DELETE})
 @RequestMapping("/api")
 public class Headquarter_Controller {
 	@Autowired
