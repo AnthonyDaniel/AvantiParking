c79ebcab1f79348258bc512f1260d9d79b7d98e0
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2019-09-25 11:08:57 -0600

    Update Parking_lot_Controller.java

--- BackEndAvantiParking/Avantiparking/src/main/java/com/avantiparking/controller/Parking_lot_Controller.java
+++ BackEndAvantiParking/Avantiparking/src/main/java/com/avantiparking/controller/Parking_lot_Controller.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.avantiparking.exception.Resource_Not_Found;
@@ -22,6 +24,8 @@ import com.avantiparking.model.Parking_lot;
 import com.avantiparking.repository.Parking_lot_Repository;
 
 @RestController
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,
+		RequestMethod.DELETE})
 @RequestMapping("/api")
 public class Parking_lot_Controller {
 	@Autowired
