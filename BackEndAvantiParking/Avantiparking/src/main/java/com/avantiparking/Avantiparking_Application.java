7127e09e957c23c96efb26fac9238da303c352c0
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2019-09-26 15:59:27 -0600

    Update Avantiparking_Application.java

--- BackEndAvantiParking/Avantiparking/src/main/java/com/avantiparking/Avantiparking_Application.java
+++ BackEndAvantiParking/Avantiparking/src/main/java/com/avantiparking/Avantiparking_Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Avantiparking_Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Avantiparking_Application.class, args);
 	}
