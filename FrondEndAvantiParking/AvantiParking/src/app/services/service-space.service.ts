498c1dc070d1b233ba06e31872619fb59139c1d5
Edrisrm <44209633+Edrisrm@users.noreply.github.com>
2019-09-30 19:21:31 -0600

    SERVICIOS DE ZONAS Y ESPACIOS
    Se agregaron los servicios de zonas y espacios

--- FrondEndAvantiParking/AvantiParking/src/app/services/service-space.service.ts
+++ FrondEndAvantiParking/AvantiParking/src/app/services/service-space.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceSpaceService {
+  baseUrl:any = "http://localhost:8080/api/space";
+  constructor(private http: HttpClient) { 
 
-  constructor() { }
+  }
+  addSpace(data:any){
+    return this.http.post(`${this.baseUrl}`,data);
+  }
+  listSpace(){
+    return this.http.get(`${this.baseUrl}`);
+  }
+  editSpace(data){
+    return this.http.put(`${this.baseUrl}`, data);
+  }
+  deleteSpace(data){
+    return this.http.delete(`${this.baseUrl}`,data)
+  }
 }
--- FrondEndAvantiParking/AvantiParking/src/app/services/service-zone.service.ts
+++ FrondEndAvantiParking/AvantiParking/src/app/services/service-zone.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceZoneService {
+  baseUrl:any = "http://localhost:8080/api/zone";
+  constructor(private http: HttpClient) { 
 
-  constructor() { }
+  }
+  addZone(data:any){
+    return this.http.post(`${this.baseUrl}`,data);
+  }
+  listZone(){
+    return this.http.get(`${this.baseUrl}`);
+  }
+  editZone(data){
+    return this.http.put(`${this.baseUrl}`, data);
+  }
+  deleteZone(data){
+    return this.http.delete(`${this.baseUrl}`,data)
+  }
 }
