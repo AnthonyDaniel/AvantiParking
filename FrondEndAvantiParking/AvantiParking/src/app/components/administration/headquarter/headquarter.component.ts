3c2612774d49aa08563064688393456e2bccfd33
kelinn <36924700+kelinn@users.noreply.github.com>
2019-10-02 08:14:02 -0600

    modals, headquarters services
    headquarters and modals of headquarters

--- FrondEndAvantiParking/AvantiParking/src/app/components/administration/headquarter/headquarter.component.ts
+++ FrondEndAvantiParking/AvantiParking/src/app/components/administration/headquarter/headquarter.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceHeadquarterService } from 'src/app/services/service-headquarter.service';
+import { Headquarter } from 'src/app/models/headquarter';
 
 @Component({
   selector: 'app-headquarter',
@@ -8,7 +9,24 @@ import { ServiceHeadquarterService } from 'src/app/services/service-headquarter.
 })
 export class HeadquarterComponent implements OnInit {
 
-  constructor(public headquarter:ServiceHeadquarterService) { 
+  constructor(public headquarter: ServiceHeadquarterService) {
+
+  }
+
+  public form = {
+    id_headquarter: null,
+    city: null,
+    name: null,
+    country: null,
+  };
+  public error: String;
+  public success: String;
+  public status: String;
+
+  filterHeadquarters;
+  public headquarters;
+
+  ngOnInit() {
     this.headquarter.listHeadquarter().subscribe(
       data => {
         console.log(data);
@@ -19,11 +37,41 @@ export class HeadquarterComponent implements OnInit {
     );
   }
 
-  filterHeadquarter;
-  public headquarters;
-  
-  ngOnInit() {
+  onSubmit() {
+    this.headquarter.addHeadquarter(this.form).subscribe(
+      data => {
+        this.responseSuccess(data);
+        this.form.id_headquarter = null;
+        this.form.name = null;
+        this.form.country = null;
+        this.form.city = null;
+      },
+      error => this.responseError(error),
+    );
 
   }
-
+  delete(headquarter: Headquarter) {
+    headquarter.id_headquarter = this.form.id_headquarter;
+    this.headquarter.deleteHeadquarter(headquarter).subscribe(
+      data => {
+        this.headquarters = this.headquarters.filter(h => h !== headquarter);
+        this.responseSuccess(data),
+          error => this.responseError(error)
+      },
+    );
+  }
+  edit(headquarter: Headquarter) {
+    this.headquarter.editHeadquarter(headquarter).subscribe(data => {
+      this.responseSuccess(data),
+        error => this.responseError(error)
+    });
+  }
+  responseSuccess(data) {
+    this.success = data.data;
+    this.status = "success";
+  }
+  responseError(error) {
+    this.error = error.error.error;
+    this.status = "error";
+  }
 }
--- FrondEndAvantiParking/AvantiParking/src/app/services/service-parking-lot.service.ts
+++ FrondEndAvantiParking/AvantiParking/src/app/services/service-parking-lot.service.ts
@@ -7,17 +7,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class ServiceParkingLotService {
   baseUrl:any = "http://localhost:8080/api/parking_lot";
+
   constructor(private http: HttpClient) { }
-  addParkingLot(data:any){
-    return this.http.post(`${this.baseUrl}`, data);
- }
- listParkingLot(){
-    return this.http.get(`${this.baseUrl}`)
- }
- editParkingLot(data){
-    return this.http.put(`${this.baseUrl}`,data);
- }
- deleteParkingLot(data){
-    return this.http.delete(`${this.baseUrl}`,data);
- }
+     addParkingLot(data:any){
+        return this.http.post(`${this.baseUrl}`, data);
+     }
+     listParkingLot(){
+        return this.http.get(`${this.baseUrl}`)
+     }
+     editParkingLot(data){
+        return this.http.put(`${this.baseUrl}`,data);
+     }
+     deleteParkingLot(data){
+        return this.http.delete(`${this.baseUrl}`,data);
+     }
+  
 }
