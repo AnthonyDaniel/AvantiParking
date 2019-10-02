5c7ac26d79b4b18026db53b73006101d79ee3904
Edrisrm <44209633+Edrisrm@users.noreply.github.com>
2019-10-01 20:05:52 -0600

    getZone( con problemas)
    Problemas al enseñar el getZone

--- FrondEndAvantiParking/AvantiParking/src/app/components/administration/zone/zone.component.ts
+++ FrondEndAvantiParking/AvantiParking/src/app/components/administration/zone/zone.component.ts
@@ -1,21 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {Zone} from '../../../models/zone';
-import {ServiceZoneService} from '../../../services/service-zone.service';
+import { ServiceZoneService } from 'src/app/services/service-zone.service';
+import { Zone } from 'src/app/models/zone';
+
 @Component({
   selector: 'app-zone',
   templateUrl: './zone.component.html',
-  styleUrls: ['./zone.component.css']
+  styleUrls: ['./zone.component.css'],
   
 })
 export class ZoneComponent implements OnInit {
-  public zone: Zone;
 
-  constructor() { }
+  public zone: Zone;
+  public zoneService;
+  constructor(public _zone:ServiceZoneService) { }
 
   ngOnInit() {
+    this.getZone();
   }
   getZone(){
-    
+    this._zone.listZone().subscribe(
+      data=>{
+        this.zoneService = data;
+        console.log(data)
+      },
+      error => console.log(error)
+    )
   }
 
 }
