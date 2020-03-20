3db71d81dc6fd5bcacd9935429ac5605d6f274f2
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2020-03-20 11:40:26 -0600

    Las nuevas credenciales para SendBlue

--- FrondEndAvantiParking/AvantiParking/src/app/app.module.ts
+++ FrondEndAvantiParking/AvantiParking/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule} from '@ng-bootstrap/ng-bootstrap'; 
 import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -57,9 +57,6 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
     VehiclePipePipe,
     ReservesComponent,
     DashboardComponent,
-   
-  
- 
   ],
   imports: [
     BrowserModule,
