03992ebcfa87d9391d10e63120cb4d30c9d12ecd
Anthony Daniel <39199700+AnthonyDaniel@users.noreply.github.com>
2020-04-16 22:20:20 -0600

    Reportes de manera  agradable

--- FrondEndAvantiParking/AvantiParking/src/app/components/administration/report/report.component.ts
+++ FrondEndAvantiParking/AvantiParking/src/app/components/administration/report/report.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceHeadquarterService } from 'src/app/services/service-headquarter.service';
+import { ServiceParkingLotService } from 'src/app/services/service-parking-lot.service';
+import { ServiceZoneService } from 'src/app/services/service-zone.service';
+import { ServiceSpaceService } from 'src/app/services/service-space.service';
 
 @Component({
   selector: 'app-report',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportComponent implements OnInit {
 
-  constructor() { }
+  private countHeadquarter = 0;
+  private countParking = 0;
+  private countZone = 0;
+  private countSpaces = 0;
+
+  constructor(public headquarter: ServiceHeadquarterService,public parking: ServiceParkingLotService,public zone: ServiceZoneService,public space: ServiceSpaceService) { }
 
   ngOnInit() {
+   this.loadDashboard();
   }
 
+  loadDashboard(){
+    this.headquarter.listHeadquarter().subscribe(
+      data=>{
+        this.loadCountHeadquarter(data);
+      }
+    );
+    this.parking.listParkingLot().subscribe(
+      data=>{
+        this.loadCountParking(data);
+      }
+    );
+    this.zone.listZone().subscribe(
+      data=>{
+        this.loadCountZones(data);
+      }
+    );
+    this.space.listSpace().subscribe(
+      data=>{
+        this.loadCountSpaces(data);
+      }
+    );
+  }
+  loadCountHeadquarter(data){
+    data.forEach(element => {
+      setTimeout(() => {
+        this.countHeadquarter++;
+      }, 700);
+    });
+  }
+  loadCountParking(data){
+    data.forEach(element => {
+      setTimeout(() => {
+        this.countParking++;
+      }, 700);
+    });
+  }
+  loadCountZones(data){
+    data.forEach(element => {
+      setTimeout(() => {
+        this.countZone++;
+      }, 700);
+    });
+  }
+  loadCountSpaces(data){
+    data.forEach(element => {
+      setTimeout(() => {
+        this.countSpaces++;
+      }, 700);
+    });
+  }
 }
